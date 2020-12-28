2f4730f28ca81b0bd6df0b6025157be6a59fcc74
Dede Rohmat H <54473030+hahahihidede@users.noreply.github.com>
2020-12-28 14:41:41 +0700

    Update testad.py

--- testad.py
+++ testad.py
@@ -1,8 +1,8 @@
 from Adafruit_IO import Client, Feed, RequestError
 
 
-ADAFRUIT_IO_KEY = 'aio_GZYX65IsgWLAnkPa8KyajYKRVUqe'
-ADAFRUIT_IO_USERNAME = 'dederohmat98'
+ADAFRUIT_IO_KEY = 'YOUR AIO KEY'
+ADAFRUIT_IO_USERNAME = 'YOUR USERNAME'
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY) 
 feed = aio.feeds('hxt')
 
