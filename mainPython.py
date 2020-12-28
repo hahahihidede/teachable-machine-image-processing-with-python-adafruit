73503a9022db8201460b37f9690bc892bddf9044
Dede Rohmat H <54473030+hahahihidede@users.noreply.github.com>
2020-12-28 14:40:56 +0700

    Update mainPython.py

--- mainPython.py
+++ mainPython.py
@@ -1,4 +1,4 @@
-
+#import neccesarry library
 import numpy as np
 import cv2
 import tensorflow.keras as tf
@@ -7,13 +7,15 @@ import math
 from Adafruit_IO import Client, Feed, RequestError
 
 if __name__ == '__main__':
+    #labels path
     labelsPath = "./model/labels.txt"
-
+    #read label.txt each line
     labelsFile = open(labelsPath, 'r')
 
-
+    #variable to store classes
     classes = []
     line = labelsFile.readline()
+    #start loop
     while line:
         
         classes.append(line.split(' ', 1)[1].rstrip())
@@ -21,14 +23,14 @@ if __name__ == '__main__':
     
     labelsFile.close()
 
-
+    #keras model path
     modelPath = './model/keras_model.h5'
     model = tf.models.load_model(modelPath, compile=False)
 
- 
+    #setup input image, in this case i use my embedded webcam as input device
     cap = cv2.VideoCapture(0)
 
-    
+    #setup frame width and height
     frameWidth = 1280
     frameHeight = 720
 
@@ -36,19 +38,20 @@ if __name__ == '__main__':
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frameHeight)
 
     cap.set(cv2.CAP_PROP_GAIN, 0)
-
+    
+    #start loop
     while True:
 
-        
+        #set printpoint to true
         np.set_printoptions(suppress=True)
 
-       
+       #reshape input image
         data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
        
         check, frame = cap.read()
        
-       
+        #setup frame
         margin = int(((frameWidth-frameHeight)/2))
         squareFrame = frame[0:frameHeight, margin:margin + frameHeight]
        
@@ -62,7 +65,7 @@ if __name__ == '__main__':
        
         data[0] = normalizedImageArray
 
- 
+        #variable to store predictiosn
         predictions = model.predict(data)
 
         
@@ -71,6 +74,7 @@ if __name__ == '__main__':
         confLabel = ""
         threshold_class = ""
        
+        #create border in frame
         per_line = 2  
         borderedFrame = cv2.copyMakeBorder(
             squareFrame,
@@ -81,12 +85,13 @@ if __name__ == '__main__':
             borderType=cv2.BORDER_CONSTANT,
             value=[0, 0, 0]
         )
-   
+        #read classes and start predict
         for i in range(0, len(classes)):
            
             confidence.append(int(predictions[0][i]*100))
            
             if (i != 0 and not i % per_line):
+                #put text in frame
                 cv2.putText(
                     img=borderedFrame,
                     text=confLabel,
@@ -109,16 +114,17 @@ if __name__ == '__main__':
                     color=(255, 255, 255)
                 )
                 confLabel = ""
-            
+            #print something in some conditions
             if confidence[i] > confThreshold:
                 threshold_class = classes[i]
                 if classes [i]== "Unknown" :
                     print("TEU NYAHO")
                 # if classes [i]== "Mangga Jelek" :
                 #     print("TIDAK MANTAP")
+          #put predicted classes in large font
         cv2.putText(img=borderedFrame,text=threshold_class,org=(int(0), int(frameHeight+20)),fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.75,color=(255, 255, 255)
         )
-
+        #show frame
         cv2.imshow("Frame", borderedFrame)
         k = cv2.waitKey(10) & 0xff 
         if k == 27:
